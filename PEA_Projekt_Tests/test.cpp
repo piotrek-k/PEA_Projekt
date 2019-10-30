438f857d63b7782cc4b272300f1d25e6f73497f0
Piotrek <kpiotrek3@gmail.com>
2019-10-30 11:22:20 +0100

    Created tests. Problem with linker

---
+++ PEA_Projekt_Tests/test.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "../PEA_Projekt/Path.h"
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(test_Path, ContainsValueReturnsTrueResult) {
+	Path* path = new Path();
+	path->InsertEdge(Edge(0, 5, 10));
+	path->InsertEdge(Edge(5, 3, 8));
+	path->InsertEdge(Edge(3, 4, 6));
+
+	EXPECT_TRUE(path->ContainsValue(5, 3));
+	EXPECT_TRUE(path->ContainsValue(3, 4));
+	EXPECT_FALSE(path->ContainsValue(3, 5));
+}
+
+TEST(test_Path, ValueCanBeConnectedToEndWorksPropely) {
+	Path* path = new Path();
+	path->InsertEdge(Edge(0, 5, 10));
+	path->InsertEdge(Edge(5, 3, 8));
+	path->InsertEdge(Edge(3, 4, 6));
+
+	EXPECT_TRUE(path->ValueCanBeConnectedToEnd(2));
+	EXPECT_TRUE(path->ValueCanBeConnectedToEnd(0));
+	EXPECT_FALSE(path->ValueCanBeConnectedToEnd(5));
+	EXPECT_FALSE(path->ValueCanBeConnectedToEnd(4));
+}
+
